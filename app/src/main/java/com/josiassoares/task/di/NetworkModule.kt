c2254f22b9b42842edb1e4e26e95e063930b9547
Josias Soares <josias.mota.soares@gmail.com>
2021-07-26 23:29:37 -0300

    Add Retrofit

---
+++ app/src/main/java/com/josiassoares/task/di/NetworkModule.kt
@@ -0,0 +1,61 @@
+package com.josiassoares.task.di
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.josiassoares.task.BuildConfig
+import com.josiassoares.task.shared.DateUtil
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideGsonBuilder(): Gson {
+        return GsonBuilder()
+            .registerTypeAdapter(Date::class.java, DateUtil.DateDeserializer())
+            .create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level =
+            if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+
+        return loggingInterceptor
+    }
+
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClientBuilder(interceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .writeTimeout(60, TimeUnit.SECONDS)
+            .addInterceptor(interceptor)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(gson: Gson, client: OkHttpClient): Retrofit.Builder {
+        return Retrofit.Builder()
+            .client(client)
+            .baseUrl(BuildConfig.URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+    }
+}
